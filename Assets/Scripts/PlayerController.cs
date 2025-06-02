440df357b2e0bb89f09d375ee18c2ce98374a284
Kinga30371 <s30371@pjwstk.edu.pl>
2025-06-02 13:49:55 +0200

    zajecia 02_06

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,47 @@ public class PlayerController : MonoBehaviour
     public float runSpeed = 7;
     public float jumpForce = 10;
     public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer spriteRenderer;
 
     public GroundChecker groundChecker;
+    public PlayerStats stats;
 
     // Start is called before the first frame update
     void Start()
     {
        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        stats = GetComponent<PlayerStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (stats.isDead) return;
+
         float moveInput = Input.GetAxis("Horizontal");
         //Debug.Log($"Input pressed: {moveInput}");
+
+        if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (moveInput != 0)
+        {
+            anim.SetBool("isWalking",true);
+        }
+        else
+        {
+            anim.SetBool("isWalking",false);
+        }
+
         if(Input.GetKey(KeyCode.LeftShift))
         {
             rb.velocity = new Vector2(moveInput * runSpeed, rb.velocity.y);
